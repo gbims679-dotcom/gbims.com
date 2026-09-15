8a2aa570ca2f73e5395c46db021982a9dd929d73
gbims679-dotcom <gbims679@gmail.com>
2026-09-15 13:19:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// GBIMS common scripts
+document.addEventListener("DOMContentLoaded", () => {
+  // Smooth active nav highlight already handled via class
+  console.log("GBIMS portal loaded");
+});
